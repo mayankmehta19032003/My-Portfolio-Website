9ab79a3d288d078ecc607eabb57d4a3d2d18f0ba
Mayank Mehta <mayankmehta880@gmail.com>
2024-09-29 04:33:35 +0530

    placed moving icons

---
+++ src/components/Target.jsx
@@ -0,0 +1,28 @@
+import { useGLTF } from '@react-three/drei'
+import { useRef } from 'react'
+import gsap from "gsap";
+import {useGSAP} from "@gsap/react";
+
+
+const Target = (props) => {
+    const tagetRef = useRef();
+    const { scene } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf');
+
+    useGSAP(()=>{
+        gsap.to(tagetRef.current.position,{
+            y: tagetRef.current.position.y + 0.5,
+            duration : 1.5,
+            repeat:-1,
+            yoyo : true
+        })
+    });
+
+
+  return (
+    <mesh {...props} ref={tagetRef}>
+        <primitive object={scene}/> 
+    </mesh>
+  )
+}
+
+export default Target
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,16 +1,20 @@
 import React, { Suspense } from 'react'
 import HackerRoom from '../components/HackerRoom';
 import { Canvas } from '@react-three/fiber';
-import { PerspectiveCamera } from '@react-three/drei';
+import { PerspectiveCamera, Ring } from '@react-three/drei';
 import CanvasLoader from '../components/CanvasLoader';
+import { useMediaQuery } from "react-responsive";
+import Target from '../components/Target';
+import ReactLogo from '../components/ReactLogo';
+import Cube from '../components/Cube';
+import Rings from '../components/Rings';
 
-import {useMediaQuery} from "react-responsive";
 
 
 const Hero = () => {
 
 
-    const isMobile = useMediaQuery({maxWidth:768});
+    const isMobile = useMediaQuery({ maxWidth: 768 });
 
     return (
         <section className='min-h-screen border-2 border-blue-500 w-full flex flex-col relative'>
@@ -19,21 +23,29 @@ const Hero = () => {
                 <p className='hero_tag text-gray_gradient'>I am a Software developer</p>
             </div>
             <div className='w-full h-full absolute inset-0'>
-               
+
                 <Canvas className="w-full h-full">
                     <Suspense fallback={<CanvasLoader/>}>
 
-                    <PerspectiveCamera makeDeafault postion={[0,0,20]}/>
-                    <HackerRoom 
-                    
-                    position={[0.1,-1.55,1.20]} 
-                    rotation={[0,-Math.PI,0]}
-                    scale={isMobile ? 0.0150 :0.0175}
-                    /> 
+                        <PerspectiveCamera makeDeafault postion={[0, 0, 20]} />
+                        <HackerRoom
 
-                    <ambientLight intensity={1}/>
-                    <directionalLight position={[10,10,10]} intensity={0.5}/>
-                    
+                            position={[0.1, -1.55, 1.20]}
+                            rotation={[0, -Math.PI, 0]}
+                            scale={isMobile ? 0.0150 : 0.0175}
+                        />
+
+                        <group>
+                            <Target position={isMobile ? [1,1, 1]: [3.5, -2, 1.20]} scale={isMobile? 0.2 :0.2} />
+                            
+                            <ReactLogo position={isMobile? [1,1,1] :[3.5, 1.5, 1.20]} />
+                            <Cube position={isMobile? [1,1,1] :[-3.5, -1.5, 1.20]}/>
+                            <Rings position={isMobile? [1,1,1] :[-11.5, 11.5, 1.20]}/>
+                        </group>
+
+                        <ambientLight intensity={1} />
+                        <directionalLight position={[10, 10, 10]} intensity={0.5} />
+
                     </Suspense>
                 </Canvas>
             </div>
