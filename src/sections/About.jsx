b274f9428330560cb53dda27e9ee63f2b39aba6e
Mayank Mehta <mayankmehta880@gmail.com>
2024-10-01 09:16:28 +0530

    completed full project section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
     }
 
     return (
-        <section className='c-space my-20'>
+        <section className='c-space my-20' id='about'>
             <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full'>
                 <div className='col-span-1 xl:row-span-3'>
                     <div className='grid-container'>
@@ -41,8 +41,8 @@ const About = () => {
                 <div className='col-span-1 xl:row-span-4'>
                     <div className='grid-container'>
                         <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
-                            <Globe height={326} width={326} backgroundColor="rgba(0,0,0,0)" backgroundImageOpacity={0.5} showAtmosphere showGraticules globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
-                                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png" />
+                            <Globe height={326} width={326} backgroundColor="rgba(0,0,0,0)" backgroundImageOpacity={0.5} showAtmosphere showGraticules globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                             bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"  />
                         </div>
                         <div>
                             <p className='grid-headtext'>
